a3c3a01a57467019c0419415647bf1765fda96e9
Andrey Solopchuk <venet.dev@gmail.com>
2017-02-15 13:31:50 +0200

    iOS fix open file

--- ios/RNFileOpener/RNFileOpener.m
+++ ios/RNFileOpener/RNFileOpener.m
@@ -15,35 +15,35 @@ RCT_REMAP_METHOD(open, filePath:(NSString *)filePath fileMine:(NSString *)fileMi
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    
+
     NSURL *fileURL = [NSURL fileURLWithPath:filePath];
-    
+
     NSFileManager *fileManager = [NSFileManager defaultManager];
     if(![fileManager fileExistsAtPath:fileURL.path]) {
         NSError *error = [NSError errorWithDomain:@"File not found" code:404 userInfo:nil];
         reject(@"File not found", @"File not found", error);
         return;
     }
-    
+
     self.FileOpener = [UIDocumentInteractionController interactionControllerWithURL:fileURL];
     self.FileOpener.delegate = self;
-    
+
     UIViewController *ctrl = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-    
-    BOOL wasOpened = [self.FileOpener presentOpenInMenuFromRect:ctrl.view.bounds inView:ctrl.view animated:YES];
-        
+
+    BOOL wasOpened = [self.FileOpener presentOptionsMenuFromRect:ctrl.view.bounds inView:ctrl.view animated:YES];
+
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
     {
         wasOpened = [self.FileOpener presentOptionsMenuFromRect:rect inView:ctrl.view animated:YES];
     }
-    
+
     if (wasOpened) {
         resolve(@"Open success!!");
     } else {
         NSError *error = [NSError errorWithDomain:@"Open error" code:500 userInfo:nil];
         reject(@"Open error", @"Open error", error);
     }
-    
+
 }
 
 @end
